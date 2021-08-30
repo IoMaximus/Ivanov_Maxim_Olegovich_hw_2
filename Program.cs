20447cd3c92b127b6fe9e745de1cfa140563756c
Иванов Максим Олегович <max3513@gmail.com>
2021-08-30 20:20:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
