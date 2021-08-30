ef40cb3a742dfd0e74405bb0d2e2f04a58327ff8
Иванов Максим Олегович <max3513@gmail.com>
2021-08-30 20:36:39 +0300

    add rating

---
+++ Rating.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace exercise_two
+{
+    class Rating
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My new rating");
+        }
+    }
+}
