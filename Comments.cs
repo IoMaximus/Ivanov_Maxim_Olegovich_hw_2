e136bf783511d9134e2a64f30da1a21ed544e825
Иванов Максим Олегович <max3513@gmail.com>
2021-08-30 20:42:50 +0300

    add comments

---
+++ Comments.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace exercise_two
+{
+    class Comments
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comment was added!");
+        }
+    }
+}
